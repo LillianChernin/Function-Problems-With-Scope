25e980cb45225ae476186c75733cd28ab4a4f48f
Lillian Chernin <l.smith1654@gmail.com>
2017-12-07 15:53:10 -0600

    create getMultipleLengths function

--- 12-07-17-Func-Prob-W-Scope.js
+++ 12-07-17-Func-Prob-W-Scope.js
@@ -18,10 +18,20 @@
 // console.log(isAVowel("a"));
 // console.log(isAVowel("s"));
 
-const getTwoLengths = (str1, str2) => {
+// const getTwoLengths = (str1, str2) => {
+//   let arrOfStringLengths = [];
+//   arrOfStringLengths.push(str1.length, str2.length);
+//   return arrOfStringLengths;
+// }
+//
+// console.log(getTwoLengths("Hank", "Hippopopalous"));
+
+const getMultipleLengths = (array) => {
   let arrOfStringLengths = [];
-  arrOfStringLengths.push(str1.length, str2.length);
+  for (let i = 0; i < array.length; i++) {
+    arrOfStringLengths.push(array[i].length);
+  }
   return arrOfStringLengths;
 }
 
-console.log(getTwoLengths("Hank", "Hippopopalous"));
+console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]));
