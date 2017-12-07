85b06558c429c878e7745a01a8b2b16a7f6c1323
Lillian Chernin <l.smith1654@gmail.com>
2017-12-07 15:45:49 -0600

    create js file

---
+++ 12-07-17-Func-Prob-W-Scope.js
@@ -0,0 +1,19 @@
+// const printCool = (name) => {
+//   return name + " is cool";
+// }
+//
+// console.log(printCool("Tinker Bell"));
+
+// const calculateCube = (num) => {
+//   return num * num * num;
+// }
+//
+// console.log(calculateCube(5));
+
+// const isAVowel = (character) => {
+//   let char = character.toLowerCase();
+//   return (char === "a" || char === "e" || char === "i" || char === "o" || char === "u")
+// }
+//
+// console.log(isAVowel("a"));
+// console.log(isAVowel("s"));
